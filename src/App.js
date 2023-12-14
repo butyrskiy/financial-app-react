056f57c156ed1f52a97cbb4449ef24283b479359
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-12-14 21:48:41 +0400

    add costs and UI components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-function App() {
+import Costs from "./components/Costs/Costs";
+
+const App = () => {
+  const costs = [
+    {
+      date: new Date(2023, 11, 14),
+      description: 'Refrigerator',
+      amount: 999.99
+    },
+    {
+      date: new Date(2023, 10, 23),
+      description: 'MacBook Pro',
+      amount: 1770.99
+    },
+    {
+      date: new Date(2023, 9, 21),
+      description: 'Car',
+      amount: 117000.00
+    },
+  ];
+
   return (
-    <h1>Start learning React!</h1>
+    <>
+      <h1>Start learning React!</h1>
+      <Costs costs={costs} />
+    </>
   );
 }
 
